142d2adfd64dd3215e6d28a8354c5f0f7a4800d3
ccg14c <ccg14c@my.fsu.edu>
2017-04-12 20:59:56 -0400

    Added Website Folder

---
+++ Website/js/search.js
@@ -0,0 +1,10 @@
+var website = {
+  
+}
+
+
+document.getElementById("websites").addEventListener("click", displayWebsites);
+
+function displayWebsites() {
+  document.getElementById("websites").style.color = "#33adff";
+}
