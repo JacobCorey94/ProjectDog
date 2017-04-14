ada47713cfb4d9625006e399c5061a8c2e9c5c43
Jacob Corey <jac15r@my.fsu.edu>
2017-04-14 00:17:31 -0400

    Amazon script has major bug, fixing soon...

--- amazon.py
+++ amazon.py
@@ -32,16 +32,66 @@ soup = bsoup(r.text, "lxml")
 # print link
 
 
-urllist = []
+urllist = []	#Stores ALL URLs on the page
+prodlist = []	#Stores only the product URLs
+spanlist = []	#Stores the <span></span> that includes the price
+pricelist = []	#Stores the prices of the products
 
 #	Grab the search results of the page itself
 #	THIS IS DEPENDENT ON THE WEBSITE ITSELF, SO SEARCH WITH SEARCH ELEMENT IN YOUR BROWSER
-results =  str(soup.find("div", {"class": "s-result-list-parent-container"}))
-#print results
+for a in soup.find_all('a', href=True):
+	if 
+	urllist.append(a['href'])
 
-while results.find("href=") != -1:
-	print results.find("href=")
-	
+for span in soup.find_all('span'):
+	if "<sup class=\"sx-price-currency\">" in str(span):
+		spanlist.append(str(span))
+
+# for x in urllist:
+# 	print x
+
+#	urllist now has EVERY URL from the page
+#	We need to filter out JUST the links to products
+for x in urllist:
+	if "https://www.amazon.com/" in x and "/dp/" in x and "#" not in x and x not in prodlist:
+		prodlist.append(x)
+
+# Quick Python Tip:
+# neato = "This is a string"
+# neato[3:] will give you "Thi"
+# neato.index('i') will return 2? RTFM
+# neato[neato.index('i'):]
+
+for y in spanlist:
+	if "<sup class=\"sx-price-currency\">" in y:
+		#print y
+		price = str(y[y.index("<sup class=\"sx-price-currency\">")+len("<sup class=\"sx-price-currency\">"): y.index("</sup>")])
+		price += str(y[y.index("sx-price-whole\">")+len("sx-price-whole\">"):y.index("</span>")])
+		price += "."
+		price += str(y[y.index("<sup class=\"sx-price-fractional\">")+len("<sup class=\"sx-price-fractional\">"):y.index("<sup class=\"sx-price-fractional\">")+len("<sup class=\"sx-price-fractional\">")+2])
+		pricelist.append(price)
+		#print price
+
+#	ISSUE: Amazon scraping has given me duplicates of every price. Let's fix this
+for x in range(len(pricelist),-1,-1):
+	if x % 2 == 1:
+		pricelist.pop(x)
+
+for z in pricelist:
+	print z
+for z in prodlist:
+	print z
+
+if len(prodlist) == len(pricelist):
+	print "YES!"
+# for prod in prodlist:
+# 	print "Found product:" + prod
+
+# part = results[results.find("s-result-item celwidget")+len("s-result-item celwidget"):results.find("</div>")+len("</div>")]
+# url = str(results[results.find("href=\"")+len("href=\""):results.find("\"title")+len("\"title")])
+# urllist.append()
+
+# print url
 #for soup.find
 	#urllist.append("".join(link.prettify().split()))
 
