22431c80464abfcf02b741d8792bcbcf1ce681ab
Jacob Corey <jac15r@my.fsu.edu>
2017-04-16 19:41:07 -0400

    More websites added to the scraping algorithm

---
+++ macys.py
@@ -0,0 +1,32 @@
+import requests
+from bs4 import BeautifulSoup as bsoup
+import sys
+import re
+
+def macys(item):
+        #print "starting"
+        item = item.replace(" ", "-")
+        url = "https://www.macys.com/shop/featured/" + item
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36'}
+        r = requests.get(url, headers=headers)
+        soup = bsoup(r.text, "html.parser")
+
+        prodlist = []
+        raw = []
+        
+        expr = "\$\d*\.\d{2}"
+        linkexpr = "(?<=href=\")(\/\S+)(?=\")"
+        c = 0
+        for li in soup.find_all('li', class_='productThumbnail borderless'):
+                raw.append(str(li))
+                
+                prodlist.append([])
+                prodlist[c].append(li.find(class_='shortDescription').text.replace("\n",""))
+                prodlist[c].append(re.findall(expr, raw[c])[len(re.findall(expr,raw[c]))-1])
+                
+                prodlist[c].append("https://www.macys.com" + re.findall(linkexpr, raw[c])[0])
+
+                c = c + 1
+
+        # return results
+        return prodlist
---
+++ staples.py
@@ -0,0 +1,38 @@
+import requests
+from bs4 import BeautifulSoup as bsoup
+import sys
+import re
+
+def staples(item):
+	#item = "usb charger"
+
+	item = item.replace(" ", "%2520")
+	url = "http://www.staples.com/fillertext/directory_" + item
+	headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36'}
+
+	r = requests.get(url, headers=headers)
+	soup = bsoup(r.text, "lxml")
+
+	prodlist = []
+	raw = []
+
+	expr = "\$\d*\.\d{2}"
+	linkexpr = "(\/\S+)(?=\")"
+
+	c = 0
+	for div in soup.find_all('div', class_='stp--new-product-tile-container desktop'):
+		raw.append(str(div))
+
+		prodlist.append([])
+		prodlist[c].append(div.find("a", class_='product-title scTrack pfm').text.strip())
+		prodlist[c].append(re.findall(expr, raw[c])[0])
+		prodlist[c].append("www.staples.com" + re.findall(linkexpr, raw[c])[0])
+
+		c = c + 1
+
+	return prodlist
+
+# for i in prodlist:
+# 	print i[0]
+# 	print i[1]
+# 	print i[2]
