76b7969190e67974084cdc7130849854735b0592
Jacob Corey <jac15r@my.fsu.edu>
2017-04-19 22:51:08 -0400

    More work on organizing tables

--- Website/js/search.js
+++ Website/js/search.js
@@ -7,12 +7,12 @@ function presentData(result) {
 		$("section").append("<section id='results'><section>");
 	}
   $("#results").append("<table id=\"resultTable\" class=\"sortable\" style=\"width:100%\">");
-  $("#resultTable").append("<tr><th>Price</th><th>Item</th></tr>");
+  $("#resultTable").append("<thead><tr><th>Price</th><th>Item</th></tr></thead><tbody>");
 	for(var ind=0; ind < result.length; ind++) {
 		//$("#results").append("<a href=\""+result[ind][2]+"\">$"+result[ind][1]+" - "+result[ind][0]+"</a><br />");
     $("#resultTable").append("<tr><td>$"+result[ind][1]+"</td><td><a href=\""+result[ind][2]+"\">"+result[ind][0]+"</a></td></tr>");
   }
-  $("#resultTable").append("</table>");
+  $("#resultTable").append("</tbody></table>");
 	$("#runDog").css("display","none");
 }
 function displayWebsites() {
