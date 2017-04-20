6f209ac3f18acf6713a1c2dee9727e2b93e5eea5
Jacob Corey <jac15r@my.fsu.edu>
2017-04-19 22:34:13 -0400

    Ordering

--- Website/js/search.js
+++ Website/js/search.js
@@ -6,13 +6,13 @@ function presentData(result) {
 		// Create it if none exists
 		$("section").append("<section id='results'><section>");
 	}
-  $("#results").append("<table class=\"sortable\" style=\"width:100%\">");
-  $("#results").append("<tr><th>Price</th><th>Item</th></tr>");
+  $("#results").append("<table id=\"resultTable\" class=\"sortable\" style=\"width:100%\">");
+  $("#resultTable").append("<tr><th>Price</th><th>Item</th></tr>");
 	for(var ind=0; ind < result.length; ind++) {
 		//$("#results").append("<a href=\""+result[ind][2]+"\">$"+result[ind][1]+" - "+result[ind][0]+"</a><br />");
-    $("#results").append("<tr><td>$"+result[ind][1]+"</td><td><a href=\""+result[ind][2]+"\">"+result[ind][0]+"</a></td></tr>");
+    $("#resultTable").append("<tr><td>$"+result[ind][1]+"</td><td><a href=\""+result[ind][2]+"\">"+result[ind][0]+"</a></td></tr>");
   }
-  $("#results").append("</table>");
+  $("#resultTable").append("</table>");
 	$("#runDog").css("display","none");
 }
 function displayWebsites() {
